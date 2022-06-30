397bcc217073ab11b6863a1c4ada98d8143cf423
Nicolas Marcora <nicolas.marcora@yahoo.co.uk>
2022-06-30 11:42:09 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,222 @@
+import './style.css'
+
+type Email = {
+  from: string
+  header: string
+  content: string
+  emailAddress: string
+  img: string
+  read: boolean
+}
+
+type State = {
+  emails: Email[]
+  selectedEmail: Email | null
+  filter: string
+}
+
+const state: State = {
+  emails: [
+    {
+      from: 'Nico',
+      header: "Link to today's video and slides is up!",
+      content:
+        'Link is up and you know where to find it! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci quo et assumenda voluptas blanditiis incidunt quia in, accusamus, qui voluptatem porro. Est reiciendis cum a architecto earum voluptatibus vel atque.',
+      emailAddress: 'nico@email.com',
+      img: 'assets/nico.JPG',
+      read: true
+    },
+    {
+      from: 'Ed',
+      header:
+        "Congratulations! You have received a free beaver! Your name will now be displayed in the classroom's beaver list!",
+      content:
+        'Beaver beaver beaver beaver beaver beaver beaver beaver ! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci quo et assumenda voluptas blanditiis incidunt quia in, accusamus, qui voluptatem porro. Est reiciendis cum a architecto earum voluptatibus vel atque.',
+      emailAddress: 'ed@email.com',
+      img: 'assets/ed.JPG',
+      read: false
+    },
+    {
+      from: 'Government',
+      header: 'Time to pay your tax!',
+      content:
+        'Pay us now! Pay us now! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci quo et assumenda voluptas blanditiis incidunt quia in, accusamus, qui voluptatem porro. Est reiciendis cum a architecto earum voluptatibus vel atque.',
+      emailAddress: 'government@email.com',
+      img: 'assets/gov.jpg',
+      read: false
+    },
+    {
+      from: 'Government',
+      header: 'You know what happens next...',
+      content: 'MOOOOO! Mwahahahaha.',
+      emailAddress: 'government@email.com',
+      img: 'assets/gov.jpg',
+      read: false
+    }
+  ],
+  selectedEmail: null,
+  filter: ''
+}
+
+function selectEmail (email: Email) {
+  email.read = true
+  state.selectedEmail = email
+}
+
+function deselectEmail () {
+  state.selectedEmail = null
+}
+
+// Q: What emails do we have? ✅ state.emails
+// Q: Should we be showing the list or the details of an email? ✅ state.selectedEmail === null ?
+// Q: If we are showing an email, which email details should we be showing? ✅ state.selectedEmail
+
+// FILTER FEATURE
+// Q: Has the user entered a filter? ✅ state.filter
+// Q: What emails should we be showing on the list? state.emails.filter
+
+function getFilteredEmails () {
+  return state.emails.filter(
+    email =>
+      email.content.toLowerCase().includes(state.filter.toLowerCase()) ||
+      email.from.toLowerCase().includes(state.filter.toLowerCase())
+  )
+}
+
+// Example of how we could render other stuff on the page
+// function renderAdverts () {
+//   let mainEl = document.querySelector('main')
+//   if (mainEl === null) return
+//   mainEl.textContent = ''
+
+//   let titleEl = document.createElement('h1')
+//   titleEl.textContent = 'ADVERTSSSS'
+
+//   mainEl.append(titleEl)
+// }
+
+// function add (a: number, b: number) {
+//   return a + b
+// }
+
+// let ASDFGHJH = 123
+// let b = 456
+
+// add(ASDFGHJH, b)
+
+function renderEmailListItem (email: Email, listEl: HTMLUListElement) {
+  let liEl = document.createElement('li')
+  liEl.className = email.read ? 'emails-list__item read' : 'emails-list__item'
+  liEl.addEventListener('click', function () {
+    selectEmail(email)
+    render()
+  })
+
+  // if (email.read) liEl.className = 'emails-list__item read'
+  // else liEl.className = 'emails-list__item'
+
+  // liEl.className = 'emails-list__item'
+  // if (email.read) liEl.classList.add('read')
+
+  let readIconEl = document.createElement('span')
+  readIconEl.className =
+    'emails-list__item__read-icon material-symbols-outlined'
+  readIconEl.textContent = email.read ? 'mark_email_read' : 'mark_email_unread'
+
+  let imgEl = document.createElement('img')
+  imgEl.className = 'emails-list__item__image'
+  imgEl.src = email.img
+
+  let fromEl = document.createElement('p')
+  fromEl.classList.add('emails-list__item__from')
+  fromEl.textContent = email.from
+
+  let contentEl = document.createElement('p')
+  contentEl.className = 'emails-list__item__content'
+  contentEl.textContent = email.header
+  liEl.append(readIconEl, imgEl, fromEl, contentEl)
+
+  listEl.appendChild(liEl)
+}
+
+function renderEmailList () {
+  let mainEl = document.querySelector('main')
+  if (mainEl === null) return
+  mainEl.textContent = ''
+
+  let titleEl = document.createElement('h1')
+  titleEl.textContent = 'Inbox'
+
+  let listEl = document.createElement('ul')
+  listEl.className = 'emails-list'
+
+  for (let email of state.emails) {
+    renderEmailListItem(email, listEl)
+  }
+
+  mainEl.append(titleEl, listEl)
+}
+
+function renderEmailDetails () {
+  let mainEl = document.querySelector('main')
+  if (mainEl === null) return
+  if (state.selectedEmail === null) return
+
+  mainEl.textContent = ''
+
+  let backButton = document.createElement('button')
+  backButton.textContent = 'BACK'
+  backButton.addEventListener('click', function () {
+    deselectEmail()
+    render()
+  })
+
+  let titleEl = document.createElement('h1')
+  titleEl.textContent = state.selectedEmail.from
+
+  let imgEl = document.createElement('img')
+  imgEl.className = 'email-details__image'
+  imgEl.src = state.selectedEmail.img
+
+  let headerEl = document.createElement('h2')
+  headerEl.className = 'email-details__header'
+  headerEl.textContent = state.selectedEmail.header
+
+  let contentEl = document.createElement('p')
+  contentEl.className = 'email-details__content'
+  contentEl.textContent = state.selectedEmail.content
+
+  mainEl.append(backButton, titleEl, imgEl, headerEl, contentEl)
+}
+
+function render () {
+  // if there is a selected email, show the details
+  // if not, show the list
+  if (state.selectedEmail) renderEmailDetails()
+  else renderEmailList()
+}
+
+function runThisOnlyAtTheStart () {
+  let logoEl = document.querySelector('.logo')
+  if (logoEl) {
+    logoEl.addEventListener('click', function () {
+      deselectEmail()
+      render()
+    })
+  }
+
+  let inputEl = document.querySelector<HTMLInputElement>('.filter-input')
+  if (inputEl) {
+    inputEl.addEventListener('keydown', function (event) {
+      if (inputEl == null) return
+      if (event.key !== 'Enter') return
+
+      state.filter = inputEl.value
+    })
+  }
+}
+
+window.state = state
+
+runThisOnlyAtTheStart()
+render()
